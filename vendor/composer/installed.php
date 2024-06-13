45862a69948e3a160891d055c6d25d97e68a3f7b
itsdr <theemrbailey@gmail,com>
2024-06-13 12:35:15 -0500

    Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'your-name/easyuibuilder',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '86f0353c98e3752cdac6d6a4266c4ebf1c612021',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'your-name/easyuibuilder' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '86f0353c98e3752cdac6d6a4266c4ebf1c612021',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
